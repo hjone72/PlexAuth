d36ae5fdd53cba6f16a595f614c7ce3819286631
hjone72 <Hayden.Jones@brisbanegrammar.com>
2016-07-05 09:24:35 +1000

    Added the modules to the include path.

--- inc/include.php
+++ inc/include.php
@@ -2,7 +2,7 @@
 	//Include all the files needed for PlexAuth
 	$path = __DIR__;
 	//Adds inc directory to includes. Ensure this actually adds the path to where your files are located. /var/www/PlexAuth/inc
-	set_include_path(get_include_path() . PATH_SEPARATOR . $path . ':' . $path . '/pages');
+	set_include_path(get_include_path() . PATH_SEPARATOR . $path . PATH_SEPARATOR . $path . '/pages' . PATH_SEPARATOR . $path . '/modules');
 	$ini_array = parse_ini_file($path."/config.ini.php"); //Config file that has configurations for site.
 	$GLOBALS['ini_array'] = $ini_array;
 	if (!isset($_SESSION)) {
