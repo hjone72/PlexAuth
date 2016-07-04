a65dd2b60040d2bc9e47f09e46a4c9e68099e379
hjone72 <Hayden.Jones@brisbanegrammar.com>
2016-07-05 09:28:10 +1000

    Improved include file.
    Creating sessions a bit more securely. Icreasing the session ID length
    and increase the randomness.

--- inc/include.php
+++ inc/include.php
@@ -6,10 +6,21 @@
 	$ini_array = parse_ini_file($path."/config.ini.php"); //Config file that has configurations for site.
 	$GLOBALS['ini_array'] = $ini_array;
 	if (!isset($_SESSION)) {
-        session_start(); 
+        session_start();
+		pa_session_regenerate();
     }
+	
+	function pa_session_regenerate($bool = false){
+		ini_set('session.hash_function', 'whirlpool');
+		ini_set('session.entropy_file', '/dev/urandom');
+		ini_set('session.entropy_length', '512');
+		ini_set('session.hash_bits_per_character', 5);
+		session_set_cookie_params(0 , '/', $GLOBALS['ini_array']['domain']);
+		session_name($GLOBALS['ini_array']['session_name']);
+		session_regenerate_id($bool);
+	}
+	
 	ini_set('display_errors', $GLOBALS['ini_array']['debug'] ? 'On' : 'Off'); //Enables or disables displaying of errors based on config file.
-	session_set_cookie_params(0 , '/', $GLOBALS['ini_array']['domain']);
 	require_once('plex_function.php');
 	require_once('PlexUser.class.php');
 	require_once('RememberMe.php');
