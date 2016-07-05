a024c899fece3f5c89ccde21e561cf4b46fae7b5
hjone72 <Hayden.Jones@brisbanegrammar.com>
2016-07-05 10:03:48 +1000

    Clear GET parameter.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	if (isset($_GET['return'])){
 		if ($_GET['return'] != "") {
 			$_SESSION['return_url'] = $_GET['return'];
+			$_GET['return'] = "";
 		}
 	}
 	require_once($path . '/inc/include.php');
