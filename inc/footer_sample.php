913588f5071ae09de6f126d99001d5efb250e54a
hjone72 <Hayden.Jones@brisbanegrammar.com>
2016-07-05 10:27:02 +1000

    Updated sample footer and nav.

--- inc/footer_sample.php
+++ inc/footer_sample.php
@@ -1,29 +1,51 @@
+<?php
+	include_once('dynamic_management.module.php');
+	$management_items = dynamicManagement($User);
+?>
 <footer class="page-footer light-blue">
 <div class="container">
-  <div class="row">
-    <div class="col l6 s12">
-      <h5 class="white-text">Your Tech Base</h5>
-      <p class="grey-text text-lighten-4">We are a team of cool kids bringing you content for your enjoyment.</p>
-
-
+    <div class="row">
+		<div class="col l6 s12">
+		  <h5 class="white-text">Your Tech Base</h5>
+		  <p class="grey-text text-lighten-4">We are a team of cool kids bringing you content for your enjoyment.</p>
+		</div>
+		<div class="col l3 m3 s4">
+			<h5 class="white-text">News</h5>
+			<ul>
+				<li><a class="white-text" href="https://getgrav.org/">Blog</a></li>
+			</ul>
+		<?php if (count($management_items) == 0) { ?>
+		</div>
+		<div class="col l3 m3 s4">
+		<?php } ?>
+			<h5 class="white-text">Help & Support</h5>
+			<ul>
+				<li><a class="white-text" href="https://getgrav.org/">FAQ</a></li>
+				<li><a class="white-text" href="https://github.com/hjone72/Bumpy-Booby">Helpdesk</a></li>
+			</ul>
+		</div>
+		<div class="col l3 m3 s4">
+			<h5 class="white-text">Features</h5>
+			<ul>
+				<li><a class="white-text" href="https://plex.tv">Plex</a></li>
+				<li><a class="white-text" href="https://github.com/tidusjar/PlexRequests.Net">Requests</a></li>
+				<li><a class="white-text" href="http://vaemendis.net/ubooquity/">Comics</a></li>
+				<li><a class="white-text" href="/?page=invite">Invite</a></li>
+			</ul>
+		</div>
+		<div class="col l3 m3 s4">
+			<?php
+				if (count($management_items) > 0) {
+					print '<h5 class="white-text">Management</h5>';
+					print '<ul>';
+					foreach ($management_items as $item => $info) {
+						print '<li><a class="white-text" href="' . $info . '">' . $item . '</a></li>';
+					}
+					print '</ul>';
+				}
+			?>
+		</div>
     </div>
-    <div class="col l3 s12">
-      <ul>
-        <li><a class="white-text" href="https://Plex.tv">Plex</a></li>
-        <li><a class="white-text" href="https://requests">Requests</a></li>
-        <li><a class="white-text" href="https://Comics.com">Comics</a></li>
-        <li><a class="white-text" href="https://htpc">HTPC-Manager</a></li>
-      </ul>
-    </div>
-    <div class="col l3 s12">
-      <ul>
-        <li><a class="white-text" href="https://sickrage">SickRage</a></li>
-        <li><a class="white-text" href="https://couchpotato">CouchPotato</a></li>
-        <li><a class="white-text" href="https://sonarr">Sonarr</a></li>
-        <li><a class="white-text" href="https://nzbget">NZBGet</a></li>
-      </ul>
-    </div>
-  </div>
 </div>
 <div class="footer-copyright">
   <div class="container">
--- inc/nav_sample.php
+++ inc/nav_sample.php
@@ -1,34 +1,72 @@
+<?php
+	include_once('dynamic_management.module.php');
+	$management_items = dynamicManagement($User);
+?>
 <div class="navbar-fixed">
  
-<ul id="dropdown" class="dropdown-content">
-  <li><a href="https://Plex.tv">Plex</a></li>
-  <li><a href="https://requests">Requests</a></li>
-  <li><a href="https://Comics.com">Comics</a></li>
-  <li><a href="https://htpc">HTPC-Manager</a></li>
-  <li><a href="https://sickrage">SickRage</a></li>
-  <li><a href="https://couchpotato">CouchPotato</a></li>
-  <li><a href="https://sonarr">Sonarr</a></li>
-  <li><a href="https://nzbget">NZBGet</a></li>
+<ul id="dropdown1" class="dropdown-content">
+	<li><a href="https://getgrav.org/">FAQ</a></li>
+	<li><a href="https://github.com/hjone72/Bumpy-Booby">Helpdesk</a></li>
+</ul>
+<ul id="dropdown2" class="dropdown-content">
+	<li><a href="https://plex.tv">Plex</a></li>
+	<li><a href="https://github.com/drzoidberg33/plexpy">Plex Statistics</a></li>
+	<li><a href="https://github.com/tidusjar/PlexRequests.Net">Requests</a></li>
+	<li><a href="http://vaemendis.net/ubooquity/">Comics</a></li>
+	<li><a href="/?page=invite">Invite</a></li>
+</ul>
+
+<?php
+	if (count($management_items) > 0) {
+		print '<ul id="dropdown3" class="dropdown-content">';
+		foreach ($management_items as $item => $info) {
+			print '<li><a href="' . $info . '">' . $item . '</a></li>';
+		}
+		print '</ul>';
+	}
+?>
+
+<ul id="dropdown4" class="dropdown-content">
+	<li><a href="/?page=signout">Sign Out</a></li>
 </ul>
 
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Your Tech Base</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a class="dropdown-button" href="#!" data-activates="dropdown">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
-        <li id="client-username"><?php print $User->getUsername()?>&nbsp;</li>
-        <li><img src="<?php print $User->getThumb()?>" alt="" class="circle" id="client-picture"></li>
-      </ul>
+		<ul class="right hide-on-med-and-down">
+			<li><a href="https://getgrav.org/">Blog</a></li>
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown1">Help & Support<i class="material-icons right">arrow_drop_down</i></a></li>
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown2">Features<i class="material-icons right">arrow_drop_down</i></a></li>
+			<?php
+				if (count($management_items) > 0) {
+			?>
+				<li><a class="dropdown-button" href="#!" data-activates="dropdown3">Management<i class="material-icons right">arrow_drop_down</i></a></li>
+			<?php
+				}
+			?>
+			<li><a class="dropdown-button" id="client-username" href="#!" data-activates="dropdown4"><?php print $User->getUsername()?>&nbsp;<img src="<?php print $User->getThumb()?>" alt="" class="circle" id="client-picture"></a></li>
+		</ul>
  
-      <ul id="nav-mobile" class="side-nav">
-          <li><a href="https://Plex.tv" target="_blank">Plex</a></li>
-          <li><a href="https://requests" target="_blank">Requests</a></li>
-          <li><a href="https://Comics.com" target="_blank">Comics</a></li>
-          <li><a href="https://htpc" target="_blank">HTPC-Manager</a></li>
-          <li><a href="https://sickrage" target="_blank">SickRage</a></li>
-          <li><a href="https://couchpotato" target="_blank">CouchPotato</a></li>
-          <li><a href="https://sonarr" target="_blank">Sonarr</a></li>
-          <li><a href="https://nzbget" target="_blank">NZBGet</a></li>
-      </ul>
+		<ul id="nav-mobile" class="side-nav">
+			<h5 class="black-text text-lighten-3 center-align">News</h5>
+				<li><a href="https://getgrav.org/">Blog</a></li>
+			<h5 class="black-text text-lighten-3 center-align">Help & Support</h5>
+				<li><a href="https://getgrav.org/faq">FAQ</a></li>
+				<li><a href="https://github.com/hjone72/Bumpy-Booby">Helpdesk</a></li>
+			<h5 class="black-text text-lighten-3 center-align">Features</h5>
+				<li><a href="https://plex.tv" target="_blank">Plex</a></li>
+				<li><a href="https://github.com/drzoidberg33/plexpy" target="_blank">Plex Statistics</a></li>
+				<li><a href="https://github.com/tidusjar/PlexRequests.Net" target="_blank">Requests</a></li>
+				<li><a href="http://vaemendis.net/ubooquity/" target="_blank">Comics</a></li>
+				<li><a href="/?page=invite">Invite</a></li>
+			<?php
+				if (count($management_items) > 0) {
+					print '<h5 class="black-text text-lighten-3 center-align">Management</h5>';
+					foreach ($management_items as $item => $info) {
+						print '<li><a href="' . $info . '" target="_blank">' . $item . '</a></li>';
+					}
+				}
+			?>
+		</ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
   </nav>
