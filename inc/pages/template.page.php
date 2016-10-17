80b83e83fa5721752a209a32bca8713170927364
hjone72 <Hayden.Jones@brisbanegrammar.com>
2016-10-17 15:30:29 +1000

    Page template
    A page template if you wish to display some info to users.

---
+++ inc/pages/template.page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!--  Meta  -->
+		<?php require_once 'inc/meta.php'; ?>
+		<title>TEMPLATE - Your Tech Base</title>
+		<!--  CSS  -->
+		<?php require_once 'inc/css.php'; ?>
+	</head>
+	<body class="grey lighten-5">
+		<!--  Nav  -->
+		<?php require_once 'inc/nav.php'; ?>
+		<main class="valign-wrapper">
+			<div class="container valign">
+				<div class="section">
+					<div class="row">
+						<div class="col s12">
+
+							<h1>Join YTB</h1>
+							<p>CONTENT GOES HERE</p>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+		<!--  Footer  -->
+		<?php require_once 'inc/footer.php'; ?>
+		<!--  Scripts  -->
+		<?php require_once 'inc/javascripts.php'; ?>
+	</body>
+</html>
