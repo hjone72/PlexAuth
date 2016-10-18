ad1c88fdab487aa66242c878d4e48a827922bf2e
hjone72 <hjone72@gmail.com>
2016-10-18 11:03:30 +1000

    Explanation as to what this file is for.

--- inc/PlexUser_ext.php
+++ inc/PlexUser_ext.php
@@ -1,3 +1,9 @@
 <?php
-
+  /* This file is only here so that you can add extras to the PlexUser.
+      For example:
+      
+      //Load users name from local DB.
+      include_once('name.module.php');
+      $this->setName(getName($this));
+      
 ?>
