6b9053a20734d25ca665e27e59ba1b9b5a17d64a
hjone72 <hjone72@gmail.com>
2016-11-15 09:59:33 +1000

    Muximux fix
    Muximux will actually work now.

--- inc/pages/main.page.php
+++ inc/pages/main.page.php
@@ -3,28 +3,25 @@
 	<head>
 		<!--  Meta  -->
 		<?php require_once 'inc/meta.php'; ?>
-		<title>Your Tech Base</title>
+		<title>PlexAuth | Secure</title>
 		<!--  CSS  -->
 		<?php require_once 'inc/css.php'; ?>
+		<?php require_once('muximux.module.php'); ?>
 	</head>
 	<body class="grey lighten-5">
 		<!--  Nav  -->
 		<?php require_once 'inc/nav.php'; ?>
-		<main>
-			<div class="container">
-				<div class="section">
-					<div class="row">
-						<div class="col s12">
-							<h1>Welcome <?php echo $User->getName()[0]; ?></h1>
-						</div>
-						<?php
-						?>
-					</div>
-				</div>
-			</div>
+		<main class="valign-wrapper">
+			<ul class="iframe-content" style="margin: 0px; width: 100%">
+				<?php
+					echo frameContent($menu_items_F);
+					echo frameContent($menu_items_HaS);
+					echo frameContent($management_items);
+				?>
+			</ul>
 		</main>
 		<!--  Footer  -->
-		<?php require_once 'inc/footer.php'; ?>
+		<?php #require_once 'inc/footer.php'; ?>
 		<!--  Scripts  -->
 		<?php require_once 'inc/javascripts.php'; ?>
 	</body>
