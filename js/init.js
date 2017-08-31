72974a2497177455fbef38ece7f37290dd75990f
hjone72 <hjone72@gmail.com>
2017-09-01 08:02:10 +1000

    Support for permissions page

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('select').material_select(); // Required for permissions page. https://github.com/hjone72/PlexAuth_Pages
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
